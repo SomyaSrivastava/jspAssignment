412cda51f641d492084e7b4515539937ec7a4e74
SomyaSrivastava <somya777.srivastava@gmail.com>
2017-07-14 15:08:22 +0530

    Upload

---
+++ src/Data.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Lenovo-1 on 13-07-2017.
+ */
+public class Data {
+    private static ArrayList<User> users=new ArrayList<User>();
+    private static Data ourInstance = new Data();
+
+    public static Data getInstance() {
+        return ourInstance;
+    }
+
+    public static void addUser(User user){
+        users.add(user);
+    }
+    public static User getUser(String name,String pswd){
+        for (int i = 0; i <users.size() ; i++) {
+            if(users.get(i).getName().equals(name))
+                if (users.get(i).getPswd().equals(pswd)) {
+                    return users.get(i);
+                }
+        }
+        return null;
+    }
+    private Data() {
+    }
+}
---
+++ src/Q1Servlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Lenovo-1 on 13-07-2017.
+ */
+public class Q1Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String n= request.getParameter("num");
+        PrintWriter out=response.getWriter();
+//        out.print("you entered : "+n);
+        int num=Integer.parseInt(n);
+        for (int i = 1; i <=num ; i++) {
+            out.print(i+" ");
+        }
+
+    }
+}
